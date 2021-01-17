a8c894d861501ff325e977850a84561b0dbd474d
ljquann <76014088+ljquann@users.noreply.github.com>
2021-01-17 15:13:24 -0700

    changed "let" to "var

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-let now=new Date();
-let ft = now.toLocaleString("en-US", {
+var now=new Date();
+var ft = now.toLocaleString("en-US", {
         hour: "numeric",
         minute: "numeric",
         hour12: true
 });
-let hour=now.getHours();
-let minute=now.getMinutes();
+var hour=now.getHours();
+var minute=now.getMinutes();
 
 if (hour<=6||  hour>=18){
     document.write(`<body style="background:rgba(119,109,138, 0.6);">`);
@@ -13,62 +13,62 @@ if (hour<=6||  hour>=18){
   }
 
 function currentDay(date){
-let days=["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
-let day=days[now.getDay()];
+var days=["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
+var day=days[now.getDay()];
   return `${day}`;
 }
 function dateAndTime(date){
-  let months=["January", "February", "March","April", "May", "June", "July", "August", "Spetember", "October", "November", "December"];
-  let month=months[now.getMonth()];
-  let dayOfMonth=now.getDate();
-  let year=now.getFullYear();
+  var months=["January", "February", "March","April", "May", "June", "July", "August", "Spetember", "October", "November", "December"];
+  var month=months[now.getMonth()];
+  var dayOfMonth=now.getDate();
+  var year=now.getFullYear();
   return `${month} ${dayOfMonth}, ${year } <br /> ${ft}`;
 }
 
-let dayOfWeek=document.querySelector("#current-day");
+var dayOfWeek=document.querySelector("#current-day");
 dayOfWeek.innerHTML=currentDay(now);
-let dayAndTime=document.querySelector("#date-time");
+var dayAndTime=document.querySelector("#date-time");
 dayAndTime.innerHTML=dateAndTime(now);
 
 
 function changeCity(event) {
   event.preventDefault();
-  let cityPlaceholder = `${cityInput.value}`;
-  let apiKey=`2ab0b590fd9866ef804df5849d5ef74a`;
-  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${cityPlaceholder}&units=imperial&appid=${apiKey}`;
+  var cityPlaceholder = `${cityInput.value}`;
+  var apiKey=`2ab0b590fd9866ef804df5849d5ef74a`;
+  var apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${cityPlaceholder}&units=imperial&appid=${apiKey}`;
   axios.get(apiUrl).then(showWeather); 
-  let forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${cityPlaceholder}&units=imperial&appid=${apiKey}`;
+  var forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${cityPlaceholder}&units=imperial&appid=${apiKey}`;
   axios.get(forecastUrl).then(showForecast);
   changeMetricUnit.classList.remove("active");
 }
 function showWeather(response){
   console.log(response);
   cityPlaceholder.innerHTML= response.data.name;
- let currentTemp=document.querySelector("#current-temp");
+ var currentTemp=document.querySelector("#current-temp");
   currentTemp.innerHTML= `${Math.round(response.data.main.temp)}`;
- let currentDescription= document.querySelector("#current-description");
+ var currentDescription= document.querySelector("#current-description");
 currentDescription.innerHTML=`${response.data.weather[0].description}`;
 }
 function showForecast(response){
   console.log(response);
-  let days=["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
+  var days=["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
   day=days[now.getDay()];
- let dayOne=new Date(response.data.list[4].dt_txt);
+ var dayOne=new Date(response.data.list[4].dt_txt);
 gotDayOne=days[dayOne.getDay()];
 
- let dayTwo=new Date(response.data.list[12].dt_txt);
+ var dayTwo=new Date(response.data.list[12].dt_txt);
 gotDayTwo=days[dayTwo.getDay()];
 
- let dayThree=new Date(response.data.list[20].dt_txt);
+ var dayThree=new Date(response.data.list[20].dt_txt);
 gotDayThree=days[dayThree.getDay()];
 
- let dayFour=new Date(response.data.list[28].dt_txt);
+ var dayFour=new Date(response.data.list[28].dt_txt);
 gotDayFour=days[dayFour.getDay()];
 
- let dayFive=new Date(response.data.list[36].dt_txt);
+ var dayFive=new Date(response.data.list[36].dt_txt);
 gotDayFive=days[dayFive.getDay()];
 
-  let forecastElement=document.querySelector("#forecast");
+  var forecastElement=document.querySelector("#forecast");
   forecastElement.innerHTML= 
   `<div class="row day-one">
             <div class="col-3 day-col">
@@ -127,50 +127,50 @@ gotDayFive=days[dayFive.getDay()];
     </div>
     </div>`;
 }
-let cityPlaceholder = document.querySelector("h1");
-let apiKey=`2ab0b590fd9866ef804df5849d5ef74a`;
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${cityPlaceholder.innerHTML}&units=imperial&appid=${apiKey}`;
+var cityPlaceholder = document.querySelector("h1");
+var apiKey=`2ab0b590fd9866ef804df5849d5ef74a`;
+var apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${cityPlaceholder.innerHTML}&units=imperial&appid=${apiKey}`;
 axios.get(apiUrl).then(showWeather);
 
-let forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${cityPlaceholder.innerHTML}&units=imperial&appid=${apiKey}`;
+var forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${cityPlaceholder.innerHTML}&units=imperial&appid=${apiKey}`;
 axios.get(forecastUrl).then(showForecast);
 
-let citySubmit = document.querySelector("#search");
+var citySubmit = document.querySelector("#search");
 citySubmit = addEventListener("submit", changeCity);
-let cityInput = document.querySelector("#cityform");
+var cityInput = document.querySelector("#cityform");
 
 function metricUnit(event){
 event.preventDefault();
-let apiKey=`2ab0b590fd9866ef804df5849d5ef74a`;
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${cityPlaceholder.innerHTML}&units=metric&appid=${apiKey}`;
+var apiKey=`2ab0b590fd9866ef804df5849d5ef74a`;
+var apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${cityPlaceholder.innerHTML}&units=metric&appid=${apiKey}`;
 axios.get(apiUrl).then(showWeather);
-let forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${cityPlaceholder.innerHTML}&units=metric&appid=${apiKey}`;
+var forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${cityPlaceholder.innerHTML}&units=metric&appid=${apiKey}`;
 axios.get(forecastUrl).then(showForecast);
 changeMetricUnit.classList.add("active");
 changeImpUnit.classList.remove("active");
 }
 function impUnit(event){
 event.preventDefault();
-let apiKey=`2ab0b590fd9866ef804df5849d5ef74a`;
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${cityPlaceholder.innerHTML}&units=imperial&appid=${apiKey}`;
+var apiKey=`2ab0b590fd9866ef804df5849d5ef74a`;
+var apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${cityPlaceholder.innerHTML}&units=imperial&appid=${apiKey}`;
 axios.get(apiUrl).then(showWeather);
-let forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${cityPlaceholder.innerHTML}&units=imperial&appid=${apiKey}`;
+var forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${cityPlaceholder.innerHTML}&units=imperial&appid=${apiKey}`;
 axios.get(forecastUrl).then(showForecast);
 changeImpUnit.classList.add("active");
 changeMetricUnit.classList.remove("active");
 }
-let changeMetricUnit= document.querySelector("#celsiuslink");
+var changeMetricUnit= document.querySelector("#celsiuslink");
 changeMetricUnit.addEventListener("click", metricUnit);
-let changeImpUnit= document.querySelector("#farenheitlink");
+var changeImpUnit= document.querySelector("#farenheitlink");
 changeImpUnit.addEventListener("click",  impUnit);
 
 function changeToGeolocation(position){
   console.log (position);
-  let latitude= position.coords.latitude;
-  let longitude= position.coords.longitude;
-  let apiKey="2ab0b590fd9866ef804df5849d5ef74a";
-  let geolocationUrl= `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
-  let forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+  var latitude= position.coords.latitude;
+  var longitude= position.coords.longitude;
+  var apiKey="2ab0b590fd9866ef804df5849d5ef74a";
+  var geolocationUrl= `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+  var forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
   changeMetricUnit.classList.remove("active");
   changeImpUnit.classList.add("active");
   axios.get(geolocationUrl).then(showWeather);   
@@ -180,5 +180,5 @@ function geoClick(event){
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(changeToGeolocation);
 }
-let currentPosition=document.querySelector("#currentlocation");
+var currentPosition=document.querySelector("#currentlocation");
 currentPosition.addEventListener("click", geoClick);
